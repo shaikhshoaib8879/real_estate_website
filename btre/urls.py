782c5fa03a3b769af8263a382ac3e1f196f32f95
shoaib shaikh <shoaib.shaikh@mirraw.com>
2023-10-31 10:29:14 +0530

    initial commit

---
+++ btre/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('listings/',include('listings.urls')),
+    path('',include('pages.urls')),
+    path('admin/', admin.site.urls),
+]
